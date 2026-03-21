30a82c297f91d24d4baed6960449088d93263d50
Sai Patil <saip60576@gmail.com>
2026-03-21 18:51:48 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import react from 'react'
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+
+  return (
+
+
+
+      <main>
+          <Navbar />
+      </main>
+
+  )
+}
+
+export default App
